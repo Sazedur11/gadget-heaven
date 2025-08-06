7e74c54989b67efc5dd8f5d9d022210c10932aa9
Sazedur <sazedur11ab@gmail.com>
2025-08-06 22:03:59 +0600

    banner design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
-        <div className="bg-[#9538E2] py-8">
+        <div className="bg-[#9538E2] pt-8 lg:pb-50 md:pb-20 pb-8 rounded-b-2xl">
             <Banner></Banner>
         </div>
     );
