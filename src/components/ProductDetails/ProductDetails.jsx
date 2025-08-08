f7df299c3543439af436d788c286451ddded1741
Sazedur <sazedur11ab@gmail.com>
2025-08-08 22:13:59 +0600

    products detailss

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { FiHeart, FiShoppingCart } from "react-icons/fi";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 
 
@@ -11,11 +12,37 @@ const ProductDetails = () => {
 
    const product = data.find(product => product.id === id)
 
+   const {gadget_name, price, about, specification, rating, availability, image} = product;
+
+   const {ram, rom, touchscreen, processor} = specification;
    
 
     return (
         <div>
-            <h2>Name: {product.price}</h2>
+            <div className="bg-themecolor pt-12 pb-40">
+                <h2 className="text-white text-2xl font-bold text-center">Product Details</h2>
+                <p className="text-white text-center my-4">Explore the latest gadgets that will take your experience to the next level. From smart devices to <br /> the coolest accessories, we have it all!</p>
+            </div>
+            <div className="md:flex gap-4 justify-center lg:w-3/6 md:w-5/6 mx-auto p-4 rounded-xl bg-base-300 -mt-30">
+                <img className="w-90 h-60 rounded-xl" src={image} alt={gadget_name} />
+                <div>
+                    <h2 className="text-2xl font-bold">{gadget_name}</h2>
+                    <p className="font-semibold my-2">Price: ${price}</p>
+                    <p className="border-1 border-green-400 w-20 h-8 flex items-center justify-center text-green-500 bg-green-400/5 p-2 rounded-full">{availability}</p>
+                    <p className="my-2">{about}</p>
+                    <p className="font-semibold mb-2">Specification:</p>
+                    <p>1. {processor}</p>
+                    <p>2. {ram}</p>
+                    <p>3. {rom}</p>
+                    <p>4. Touchscreen: {touchscreen}</p>
+                    <div className="flex gap-4 items-center mt-4">
+                    <p className="my-2"> Rating: {rating}</p>
+                        <button className="flex items-center gap-2 bg-themecolor py-2 px-3 rounded-full text-white">Add To Card <FiShoppingCart></FiShoppingCart></button>
+                        <button className="bg-white text-black text-2xl rounded-full p-2"> <FiHeart></FiHeart> </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
